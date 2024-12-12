085baf5cd0b9ceb48858bcd1bc99abbe37fe2db2
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-12 21:05:20 +0500

    Инициализированы переменные и написан метод Help. Kochetov

--- pr5_Kochetov_Lipina/Program.cs
+++ pr5_Kochetov_Lipina/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace pr5_Kochetov_Lipina
 {
-     class Program
+    public class Program
     {
+        static IPAddress ServerIPAddress;
+        static int ServerPort;
+        static int MaxClient;
+        static int Duration;
+        static List<Client> AllClients = new List<Client>();
         public void Main(string[] args)
         {
+        }
+        static void Help()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Command to the clients: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/config");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("  - set initial settings");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/disconnect");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - disconnect users from server");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/status");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("  - show list users");
         }
     }
 }
